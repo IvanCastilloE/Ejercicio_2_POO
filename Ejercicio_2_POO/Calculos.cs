f3e1388e79d534b3cf3c904f6c9b790442503564
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-01-28 14:21:50 -0700

    Agregar archivos de proyecto.

---
+++ Ejercicio_2_POO/Calculos.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ejercicio_2_POO
+{
+    class Calculos
+    {
+        //Definicion de variables privadas
+        private double _Numero1;
+        private double _Numero2;
+        
+        public double Numero1
+        {
+            get { return _Numero1; } //Me ayuda a obtener el valor de la variable
+            set { _Numero1 = value; } //Me ayuda a asignarle el valor a la variable
+        }
+        public double Numero2
+        {
+            get { return _Numero2; }
+            set { _Numero2 = value; }
+        }
+        public double Sumar()
+        {
+            return Numero1 + Numero2;
+        }
+        public double Restar()
+        {
+            return Numero1 - Numero2;
+        }
+        public double Multiplicar()
+        {
+            return Numero1 * Numero2;
+        }
+        public object Dividir()
+        {
+            if (Numero2 != 0)
+            {
+                return Numero1 / Numero2;
+            }
+            else
+            {
+                return "Jaja no funca si divides entre 0";
+            }
+        }
+
+
+    }
+}
---
+++ Ejercicio_2_POO/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe en el numero de la operacion a realizar: \n1 - Sumar\n2 - Restar\n3 - " +
+                "Multiplicar\n4 - Dividir");
+
+            String opcion = Console.ReadLine();
+
+            if (opcion.Equals("1") || opcion.Equals("2") || opcion.Equals("3") || opcion.Equals("4"))
+            {
+                Calculos Resultado = new Calculos();
+                Console.Write("Ingrese el primer número: ");//Pide el primer numero
+                Resultado.Numero1 = double.Parse(Console.ReadLine());//Leer el numero ingresado y asignarlo a la variable
+
+                Console.Write("Ingrese el segundo número: ");//Pide el segundo numero
+                Resultado.Numero2 = double.Parse(Console.ReadLine());//Leer el segundo numero 
+
+                if (opcion.Equals("1"))
+                {
+                    Console.WriteLine(Resultado.Sumar());
+                }
+                if (opcion.Equals("2"))
+                {
+                    Console.WriteLine(Resultado.Restar());
+                }
+                if (opcion.Equals("3"))
+                {
+                    Console.WriteLine(Resultado.Multiplicar());
+                }
+                if (opcion.Equals("4"))
+                {
+                    Console.WriteLine(Resultado.Dividir());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Jajaja que pedo, ese numero no es opcion");
+            }
+            Console.ReadKey();
+        }
+    }
+}
